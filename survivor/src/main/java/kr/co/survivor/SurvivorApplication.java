3d4529416fe9e80dbf84469f039a9103493583fd
yamyame03 <yamyame03@naver.com>
2024-06-18 13:11:49 +0900

    2024-06-18

---
+++ survivor/src/main/java/kr/co/survivor/SurvivorApplication.java
@@ -0,0 +1,13 @@
+package kr.co.survivor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurvivorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurvivorApplication.class, args);
+	}
+
+}
---
+++ survivor/src/test/java/kr/co/survivor/SurvivorApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.survivor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurvivorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
