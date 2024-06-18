bcaac9526d244d68ffbf058ca2c43b81aaf46123
yamyame03 <yamyame03@naver.com>
2024-06-18 13:10:04 +0900

    2024

--- src/main/java/kr/co/survive/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.co.survive.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kr.co.survive.entity.MemberEntity;
-
-public interface MemberRepository extends JpaRepository<MemberEntity, String>{
-	public MemberEntity findByUsername(String username);
-}
